768e0a28b44bb23216a513d5eb62e9e87ced7e0c
Sarah Böning <Sarah.boening@tu-clausthal.de>
2018-05-03 23:04:59 +0200

    Added graphview and display of acc. sensor data in real time

--- app/src/main/java/com/example/mis/sensor/MainActivity.java
+++ app/src/main/java/com/example/mis/sensor/MainActivity.java
@@ -1,30 +1,103 @@
 package com.example.mis.sensor;
 
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import com.example.mis.sensor.FFT;
+import android.widget.Toast;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.Viewport;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     //example variables
     private double[] rndAccExamplevalues;
     private double[] freqCounts;
+
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    double ax, ay, az, aMag;
+    float time;
+    LineGraphSeries<DataPoint> lineX, lineY, lineZ, lineMag;
+
+    GraphView graphViewAcc, graphViewFFT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if(sensor == null){
+            makeErrorLog("Accelerometer not available");
+        } else{
+            graphViewAcc = findViewById(R.id.graphViewAcc);
+            graphViewFFT = findViewById(R.id.graphViewFFT);
+
+            graphViewAcc.setBackgroundColor(Color.GRAY);
+
+            Viewport vp = graphViewAcc.getViewport();
+            vp.setScalable(true);
+            vp.setScalableY(true);
+            vp.setScrollableY(true);
+            vp.setScrollable(true);
+
+
+            lineX = new LineGraphSeries<>();
+            lineY = new LineGraphSeries<>();
+            lineZ = new LineGraphSeries<>();
+            lineMag = new LineGraphSeries<>();
+
+            lineX.setColor(Color.RED);
+            lineY.setColor(Color.GREEN);
+            lineZ.setColor(Color.BLUE);
+            lineMag.setColor(Color.WHITE);
+
+            graphViewAcc.addSeries(lineX);
+            graphViewAcc.addSeries(lineY);
+            graphViewAcc.addSeries(lineZ);
+            graphViewAcc.addSeries(lineMag);
+/*
         //initiate and fill example array with random values
         rndAccExamplevalues = new double[64];
         randomFill(rndAccExamplevalues);
-        new FFTAsynctask(64).execute(rndAccExamplevalues);
+        new FFTAsynctask(64).execute(rndAccExamplevalues); */
+    }
     }
 
+    @Override
+    public final void onAccuracyChanged(Sensor sensor, int accuracy){
+
+    }
+
+    @Override
+    public final void onSensorChanged(SensorEvent event) {
+          updateGraph(( event.timestamp/100000000), event.values[0], event.values[1], event.values[2], magnitude(event.values[0], event.values[1], event.values[2]));
+
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
 
     /**
      * Implements the fft functionality as an async task
@@ -32,6 +105,17 @@ public class MainActivity extends AppCompatActivity {
      * fft(double[] x, double[] y)
      */
 
+    private void updateGraph(final long timestamp, final float x, final float y, final float z, final float mag){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                lineX.appendData(new DataPoint(timestamp, x), true, 40);
+                lineY.appendData(new DataPoint(timestamp, y), true, 40);
+                lineZ.appendData(new DataPoint(timestamp, z), true, 40);
+                lineMag.appendData(new DataPoint(timestamp, mag), true, 40);}
+        });
+
+    }
     private class FFTAsynctask extends AsyncTask<double[], Void, double[]> {
 
         private int wsize; //window size must be power of 2
@@ -74,9 +158,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
-
-
     /**
      * little helper function to fill example with random double values
      */
@@ -87,6 +168,14 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public float magnitude(float x, float y, float z){
+        return (float) Math.sqrt(Math.pow(x,2) + Math.pow(y,2) + Math.pow(z,2));
+    }
 
+    // Simple function to display and log errors/exceptions
+    public void makeErrorLog(String error) {
+        Log.e("MainActivity", error);
+        Toast.makeText(MainActivity.this, error, Toast.LENGTH_SHORT).show();
+    }
 
 }
